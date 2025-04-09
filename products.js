e2b918f6f22f329418a662d46f90efaefcf5052e
lpx0007 <2305484273@qq.com>
2025-04-09 21:26:15 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,17 @@
+const products = [
+    {
+        id: 1,
+        title: "光子嫩肤",
+        category: "光电",
+        price: "¥1280.00",
+        image: "images/光子嫩肤.jpg"
+    },
+    {
+        id: 2,
+        title: "中胚层疗法",
+        category: "中胚",
+        price: "¥1680.00",
+        image: "images/中胚层疗法.jpg"
+    }
+    // 添加其他产品...
+];
